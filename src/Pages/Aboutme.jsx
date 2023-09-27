85928e3f229e66425293e210af3d7b07c76e3b09
valentinacamacho <valentinacamacho0206@gmail.com>
2023-09-26 21:36:50 -0500

    I added an element in the aboutme module.

---
+++ src/Pages/Aboutme.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Style/sheetsStyle.css";
+
+function Aboutme() {
+  return (
+    <div className="container-About">
+      <div className="About-image">
+
+      </div>
+      <div className="cabout-Info">
+        <p>Hola! Soy Jenifer Camacho. Soy analista y desarrolladora web, soy una mujer dedicada, apasionada e innovadora.</p>
+        <p>Aspiro ampliar mis conocimientos y habilidades dentro de una entidad que me brinde la oportunidad de desarrollar mis habilidades de manera amplia y al mismo tiempo contribuir al crecimiento competitivo de la empresa.</p>
+      </div>
+      <div className="boton">
+       <button>Visualizar CV</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Aboutme;
